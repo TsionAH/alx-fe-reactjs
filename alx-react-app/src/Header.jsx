d2dbb1770fe606bffdc05ef38ed8c80a190dd05c
TsionAH <tsionalemuhandiso@gmail.com>
2025-08-31 09:00:05 -0700

    delete the vite and react logo

--- alx-react-app/src/Header.jsx
+++ alx-react-app/src/Header.jsx
@@ -2,8 +2,8 @@ function Header(){
     return(
         
             <header>
-  <h1>My Favorite Cities</h1>
-</header>
+                <h1>My Favorite Cities</h1>
+            </header>
         
     );
 }
