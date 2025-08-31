9ba4331c653553391a6a9e563c67604a70e96b60
TsionAH <tsionalemuhandiso@gmail.com>
2025-08-31 13:40:23 -0700

    new

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -13,9 +13,9 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
-      <WelcomeMessage />
+      {/* <WelcomeMessage />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-     
+      */}
       
       
       
