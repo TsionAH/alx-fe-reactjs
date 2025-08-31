43a63e9d2de13fb4f7cd6738229c6cc2d287c016
TsionAH <tsionalemuhandiso@gmail.com>
2025-08-31 13:57:13 -0700

    change it

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,26 +2,15 @@ import React from 'react';
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
-// import WelcomeMessage from './components/WelcomeMessage';
-// import UserProfile from './components/UserProfile';
 
 function App() {
- 
   return (
     <>
-     
       <Header />
       <MainContent />
       <Footer />
-      {/* <WelcomeMessage />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      */}
-      
-      
-      
     </>
-    
   );
 }
 
-export default App
+export default App;
